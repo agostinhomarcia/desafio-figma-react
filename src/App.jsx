c67e131bb8218c550f6f1ae8d3281930ac86ac64
Marcia Agostinho <marciaagostinho27@gmail.com>
2022-10-31 21:11:24 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import  './App.css'
+
+
+import {
+  
+  Routes,
+  Route,
+} from "react-router-dom";
+
+import Icon1 from '../src/assets/img/foto1.png'
+import Icon2 from '../src/assets/img/foto2.png'
+import Icon3 from '../src/assets/img/foto3.png'
+import Icon4 from '../src/assets/img/foto4.png'
+import Icon5 from '../src/assets/img/foto5.png'
+import Icon6 from '../src/assets/img/foto6.png'
+import Icon7 from '../src/assets/img/foto7.png'
+import Icon8 from '../src/assets/img/foto8.png'
+import Icon9 from '../src/assets/img/foto9.png'
+import Icon10 from '../src/assets/img/foto10.png'
+import Icon11 from '../src/assets/img/foto11.png'
+import Icon12 from '../src/assets/img/foto12.png'
+import Icon13 from '../src/assets/img/foto13.png'
+import Icon14 from '../src/assets/img/foto14.png'
+import Icon15 from '../src/assets/img/foto15.png'
+
+function App() {
+  return (
+    <>
+    
+
+    <div className='container-icons'>
+      <img className='icon' src={Icon1} alt="icon1" />
+      <img className='icon' src={Icon2} alt="icon2" />
+      <img className='icon' src={Icon3} alt="icon3" />
+      <img className='icon' src={Icon4} alt="icon4" />
+      <img className='icon' src={Icon5} alt="icon5" />
+      <img className='icon' src={Icon6} alt="icon6" />
+      <img className='icon' src={Icon7} alt="icon7" />
+      <img className='icon' src={Icon8} alt="icon8" />
+      <img className='icon' src={Icon9} alt="icon9" />
+      <img className='icon' src={Icon10} alt="icon10" />
+      <img className='icon' src={Icon11} alt="icon11" />
+      <img className='icon' src={Icon12} alt="icon12" />
+      <img className='icon' src={Icon13} alt="icon13" />
+      <img className='icon' src={Icon14} alt="icon14" />
+      <img className='icon' src={Icon15} alt="icon15" />
+
+
+     
+
+
+    </div>
+  </>
+
+
+  )
+}
+
+export default App
